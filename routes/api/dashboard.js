6e5752d58906fd2d991cc1acd5e1ce6c0071cea5
Arnold van der Merwe <arnold1.vandermerwe@gmail.com>
2023-09-19 19:26:07 +0200

    routes/api/dashboard: Added route for admin and improved readability
    Added a new route for an admin dashboard page to fetch the correct
    data. Some of the code was split into functions for readability.
    
    Signed-off-by: Arnold van der Merwe <arnold1.vandermerwe@gmail.com>

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -4,7 +4,7 @@ const db = require("../../lib/mariadb.js");
 const influxdb = require("../../lib/influxdb");
 
 // Get general information for home dashboard
-router.get("/:id", async (req, res) => {
+router.get("/homeowner/:id", async (req, res) => {
   // Fetch home info from MariaDB
   let homeQuery = await (
     await db
@@ -37,40 +37,18 @@ router.get("/:id", async (req, res) => {
     generalQuery.find((obj) => obj.field === "load_limit_home").value
   );
   if (!loadControl) {
-    loadLimit = "None";
+    loadLimit = "Inactive";
   }
 
   // Fetch electrical rates
   let ratesQuery = await (await db).query("select * from electrical_rate");
-  let rate = 0;
-  for (row of ratesQuery) {
-    let valid = betweenTimes(row.start_time, row.end_time);
-    if (valid) {
-      if (loadshedding === "Active") {
-        rate = row.loadshedding_rate;
-      } else {
-        rate = row.normal_rate;
-      }
-    }
-  }
+  let rate = getRate(ratesQuery, loadshedding);
 
   // Fetch measurements
   let powerQuery = await influxdb.query(
     `select * from power where device = 'home-${home.home_number}' order by time desc limit 1`
   );
-  let power = "No data";
-  if (powerQuery[0] !== undefined) {
-    // Check if measurement is within last minute
-    const date = new Date(powerQuery[0].time);
-    const timeNow = Date.now();
-    const timeDifference = timeNow - date;
-    const timeDifferenceInMinutes = timeDifference / (1000 * 60);
-    if (timeDifferenceInMinutes < 1) {
-      power = powerQuery[0].value;
-    } else {
-      power = "Offline";
-    }
-  }
+  let power = getPower(powerQuery);
 
   // Format response
   let resObj = {
@@ -82,8 +60,88 @@ router.get("/:id", async (req, res) => {
     load_limit: loadLimit,
   };
 
-  res.send(resObj);
+  return res.send(resObj);
 });
+
+// Get general information for admin dashboard
+router.get("/admin", async (req, res) => {
+  // Fetch general info (loadshedding, load limit)
+  let generalQuery = await (await db).query("select * from general");
+  // Parse into true or false
+  let loadControl = JSON.parse(
+    generalQuery.find((obj) => obj.field === "load_control").value
+  );
+  let loadshedding = JSON.parse(
+    generalQuery.find((obj) => obj.field === "loadshedding").value
+  );
+  // Get correct string for load shedding status
+  if (loadshedding === false) {
+    loadshedding = "Inactive";
+  } else {
+    loadshedding = "Active";
+  }
+  // Parse into number
+  let loadLimit = parseFloat(
+    generalQuery.find((obj) => obj.field === "load_limit_community").value
+  );
+  if (!loadControl) {
+    loadLimit = "Inactive";
+  }
+
+  // Fetch electrical rates
+  let ratesQuery = await (await db).query("select * from electrical_rate");
+  let rate = getRate(ratesQuery, loadshedding);
+
+  // Fetch measurements
+  let powerQuery = await influxdb.query(
+    `select * from power where device = 'community' order by time desc limit 1`
+  );
+  let power = getPower(powerQuery);
+
+  // Format response
+  let resObj = {
+    power: power,
+    current_rate: rate,
+    loadshedding: loadshedding,
+    load_limit: loadLimit,
+  };
+
+  return res.send(resObj);
+});
+
+// Get correct value for rate
+function getRate(query, loadshedding) {
+  let rate = 0;
+  for (row of query) {
+    let valid = betweenTimes(row.start_time, row.end_time);
+    if (valid) {
+      if (loadshedding === "Active") {
+        rate = row.loadshedding_rate;
+      } else {
+        rate = row.normal_rate;
+      }
+    }
+  }
+  return rate;
+}
+
+// Get correct value for power
+function getPower(query) {
+  let power = "No data";
+  if (query[0] !== undefined) {
+    // Check if measurement is within last minute
+    const date = new Date(query[0].time);
+    const timeNow = Date.now();
+    const timeDifference = timeNow - date;
+    const timeDifferenceInMinutes = timeDifference / (1000 * 60);
+    if (timeDifferenceInMinutes < 1) {
+      power = query[0].value;
+    } else {
+      power = "Offline";
+    }
+  }
+  return power;
+}
 
 // Function to check if current time is between two time slots.
 function betweenTimes(startTime, endTime) {
